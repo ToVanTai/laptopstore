8a0fd7e9c371227bc53b4d9e8a59355c8b1ca3ca
ToVanTai <tovantaidz2001@gmail.com>
2024-05-14 20:27:59 +0700

    email  đổi mật khẩu

--- enum/EmailUserType.php
+++ enum/EmailUserType.php
@@ -6,5 +6,6 @@
     final class EmailUserType{
         public const REGISTER = 1;//ĐĂNG KÝ TÀI KHOẢN
         public const REST_PASSWORD = 2;//QUÊN MẬT KHẨU
+        public const CHANGE_PASSWORD = 3;//ĐỔI MẬT KHẨU
     }
 ?>
---
+++ model/UserEmailChangePassword.php
@@ -0,0 +1,30 @@
+<?php
+namespace laptopstore\model;
+    /**
+     * thông tin giỏ hàng trong session
+     */
+    class UserEmailChangePassword {
+        public $account;
+        public $hashedPassword;
+        public $type;
+        
+        
+        public function __construct($data = null) {
+            if ($data !== null) {
+                $this->account = $data['account'];
+                $this->hashedPassword = $data['hashedPassword'];
+                $this->type = $data['type'];                
+            }
+        }
+        /**
+         * trả về thông tin của session
+         */
+        public function getInfo(){
+            return array(
+                'account' => $this->account,
+                'hashedPassword' => $this->hashedPassword,
+                'type' => $this->type,
+            );
+        }
+    }
+?>
